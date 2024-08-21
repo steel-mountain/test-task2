e77629f713070d01f574f364d3c334fdce7bf010
oipik <wazuup.lw@mail.ru>
2024-08-21 23:02:14 +0300

    move BrowserRouter to Pages, to exclude rerender Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Companies from "./companies/Companies";
 import Employees from "./employees/Employees";
 
 const Pages: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Companies />} />
-      <Route path="/:company/employees" element={<Employees />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Companies />} />
+        <Route path="/:company/employees" element={<Employees />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
